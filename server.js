1b4e12f00c1947bb8828d9cd25632828253df99f
Ron Norman <normalnorman@gmail.com>
2017-03-20 21:45:42 -0700

    updating default message

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = 8080;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello, container world\n');
+app.get('/', function(req, res) {
+    res.send('Hello, brave new containerized world\n');
 });
 
 app.listen(PORT);
